dfb16ad6e4318a17a586951159bb58a86c4b5b30
bossarasan18 <knowledgking425@gmail.com>
2026-04-19 19:44:42 +0530

    Add files via upload

---
+++ Final-React/src/pages/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import AboutUs from "./pages/AboutUs";
+import Services from "./pages/Services";
+import ContactUs from "./pages/ContactUs";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/" element={<h1>Welcome to Paradise Nursery</h1>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
